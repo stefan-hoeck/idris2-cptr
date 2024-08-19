1065b00bf64485c91dca4fe02231197a90f75395
stefan-hoeck <hock@zhaw.ch>
2024-08-19 12:37:16 +0200

    [ CI ] explicit return 0 in cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -2,9 +2,10 @@
 
 echo "cleaning up generated code"
 
-if [ "$(
-    git status >/dev/null 2>&1
-    echo $?
-)" -eq 0 ]; then
+if [ "$(git status >/dev/null 2>&1; echo $?)" -eq 0 ]; then
     git restore src/Data/C/Integer.idr
 fi
+
+echo "cleaned up generated code"
+
+exit 0
