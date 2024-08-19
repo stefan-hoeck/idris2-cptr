a48cd96669da30c321f6eb661c0e68ac608f1f5f
stefan-hoeck <hock@zhaw.ch>
2024-08-19 12:57:58 +0200

    [ fix ] dont make cleanup dependent on git

--- cleanup.sh
+++ cleanup.sh
@@ -2,10 +2,7 @@
 
 echo "cleaning up generated code"
 
-if [ "$(git status >/dev/null 2>&1; echo $?)" -eq 0 ]; then
-    git restore src/Data/C/Integer.idr
-fi
+mv tmp/Integer.idr src/Data/C/Integer.idr
+rm -r tmp
 
 echo "cleaned up generated code"
-
-exit 0
