9e933ea0e085e9f71f0b61f2272acd75431b424d
stefan-hoeck <hock@zhaw.ch>
2024-08-24 17:36:40 +0200

    [ C ] added some more utilities

--- support/cptr.c
+++ support/cptr.c
@@ -1,5 +1,26 @@
 #include<stdio.h>
 #include<stdint.h>
+#include<stdlib.h>
+
+void * cptr_malloc(size_t bytes) {
+    if (bytes == 0) {
+        return NULL;
+    } else {
+        return malloc(bytes);
+    }
+}
+
+void * cptr_calloc(size_t n, size_t size) {
+    if (n == 0) {
+        return NULL;
+    } else {
+        return calloc(n, size);
+    }
+}
+
+void * cptr_free(void * ptr) {
+    free(ptr);
+}
 
 char * cptr_inc_ptr(char * arr, size_t pos) {
     return arr + pos;
@@ -67,4 +88,8 @@ void *cptr_set_int32(void * ptr, int32_t v) {
 
 void *cptr_set_int64(void * ptr, int64_t v) {
     ((int64_t *) ptr)[0] = v;
+}
+
+struct timespec * cptr_allocTimespec() {
+    return cptr_calloc(1, sizeof(struct timespec));
 }
