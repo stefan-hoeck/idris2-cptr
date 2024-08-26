3ee30bac428af3b9f6d688f71d4d026f4073d988
stefan-hoeck <hock@zhaw.ch>
2024-08-26 11:48:55 +0200

    [ new ] add OffT integer type

--- codegen/integer.c
+++ codegen/integer.c
@@ -37,6 +37,7 @@ void *main() {
   utype("SizeT", sizeof(size_t));
 
   stype("ModeT", sizeof(mode_t));
+  stype("OffT", sizeof(off_t));
   stype("TimeT", sizeof(time_t));
   stype("SusecondsT", sizeof(suseconds_t));
   stype("NsecT", sizeof(struct timespec) - sizeof(time_t));
