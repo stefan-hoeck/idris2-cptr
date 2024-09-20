8ff11fef2f349770dacb3a5cf7f928c62117c218
stefan-hoeck <hock@zhaw.ch>
2024-09-20 09:51:45 +0200

    [ lint ] use clang format

--- support/cptr.c
+++ support/cptr.c
@@ -23,7 +23,7 @@ void *cptr_calloc(size_t n, size_t size) {
 
 void *cptr_free(void *ptr) { free(ptr); }
 
-char *cptr_inc_ptr(char *arr, size_t n, size_t pos) { return arr + n*pos; }
+char *cptr_inc_ptr(char *arr, size_t n, size_t pos) { return arr + n * pos; }
 
 uint8_t get_bits8(uint8_t *ptr, uint32_t ix) { return ptr[ix]; }
 
